3ef24e58e8c898a4a2810ab2b28148f455a764c6
dan <you@example.com>
2023-07-14 16:52:03 +0300

    added email js

--- script.js
+++ script.js
@@ -19,3 +19,44 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+//   Email Js
+
+function sendMail() {
+    var params = {
+      email: document.getElementById("email").value,
+    };
+
+    const serviceID = "service_5ozc1eg";
+    const templateID = "template_4ocb9er";
+
+    emailjs.send(serviceID, templateID, params)
+    // .then(res=>{
+    //     document.getElementById("email").value = "";
+    //     console.log(res);
+    //     alert("Your message sent successfully!!")
+
+    // })
+    .catch(err=>console.log(err));
+
+}
+
+
+
+
+
+
+const successButton = document.getElementById('successButton');
+const successPopup = document.getElementById('successPopup');
+const closeButton = document.getElementById('closeButton');
+
+successButton.addEventListener('click', function() {
+  setTimeout(function() {
+    successPopup.classList.remove('hidden');
+  }, 1000); // Delay of 2 seconds (2000 milliseconds)
+});
+
+closeButton.addEventListener('click', function() {
+  successPopup.classList.add('hidden');
+});
